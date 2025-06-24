f712b6593bab18ff7b836b6b5d2a09134a200347
ShohanNur <shohanur.rahman133883@gmail.com>
2025-06-25 01:02:27 +0600

    updated

--- samsung_warhole.cpp
+++ samsung_warhole.cpp
@@ -1,95 +1,91 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<map>
+#include<climits>
+
+#define  ll     long long
+#define  endl   "\n"
+#define  pb     push_back
+#define  f      first
+#define  s      second
+#define  inf    1e18  
 using namespace std;
 
-#define pii pair<int, int>
-#define piii pair<int, pii>
+map<pair<ll,ll>, vector<pair<pair<ll,ll>,ll>> > mp;
+map<pair<ll,ll>, ll>vis;
 
-int manhattan(pii a, pii b) {
-    return abs(a.first - b.first) + abs(a.second - b.second);
+ll dist(pair<ll,ll>&a , pair<ll,ll>&b){
+     return abs(a.f-b.f) + abs(a.s-b.s);
 }
-
-int solve(int n, pii src, pii dest, vector<tuple<pii, pii, int>>& wormholes) {
-    vector<pii> nodes;
-    nodes.push_back(src);
-    nodes.push_back(dest);
-
-    for (auto& wh : wormholes) {
-        nodes.push_back(get<0>(wh));
-        nodes.push_back(get<1>(wh));
-    }
-
-    int sz = nodes.size();
-    vector<vector<pair<int, int>>> adj(sz);
-
-    // Add direct travel edges
-    for (int i = 0; i < sz; i++) {
-        for (int j = 0; j < sz; j++) {
-            if (i != j) {
-                int d = manhattan(nodes[i], nodes[j]);
-                adj[i].push_back({j, d});
-            }
-        }
-    }
-
-    // Add wormhole edges (bi-directional)
-    for (int i = 0; i < wormholes.size(); i++) {
-        pii a = get<0>(wormholes[i]);
-        pii b = get<1>(wormholes[i]);
-        int cost = get<2>(wormholes[i]);
-
-        int ai = find(nodes.begin(), nodes.end(), a) - nodes.begin();
-        int bi = find(nodes.begin(), nodes.end(), b) - nodes.begin();
-
-        adj[ai].push_back({bi, cost});
-        adj[bi].push_back({ai, cost});
-    }
-
-    // Dijkstra from src (index 0) to dest (index 1)
-    vector<int> dist(sz, INT_MAX);
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-
-    dist[0] = 0;
-    pq.push({0, 0});
-
-    while (!pq.empty()) {
-        auto [d, u] = pq.top();
-        pq.pop();
-
-        if (d > dist[u]) continue;
-
-        for (auto [v, w] : adj[u]) {
-            if (dist[v] > dist[u] + w) {
-                dist[v] = dist[u] + w;
-                pq.push({dist[v], v});
-            }
-        }
-    }
-
-    return dist[1];
+void solve(){
+      
+      int n;
+      cin>>n;
+      int s1,s2,d1,d2;
+      cin>>s1>>s2>>d1>>d2;
+      vector<pair<ll,ll>>nodes; 
+      pair<ll,ll>src = {s1,s2};
+      pair<ll,ll>des = {d1,d2};
+      nodes.pb(src);
+      nodes.pb(des);
+    
+    
+      for(int i=0;i<n;i++){
+            int a,b,c,d,cost;
+            cin>>a>>b>>c>>d>>cost;
+            pair<ll,ll>ind = {a,b};
+            pair<ll,ll>outd = {c,d};
+            mp[ind].pb({outd,cost});
+            mp[outd].pb({ind,cost});
+            nodes.pb(ind);
+            nodes.pb(outd);
+      }
+      map<pair<ll,ll>, ll>dis; 
+      for(auto i:nodes){
+          dis[i] = INT_MAX;
+      }    
+      
+     priority_queue< pair<ll,pair<ll,ll>> , vector<pair<ll,pair<ll,ll>>> , greater<pair<ll,pair<ll,ll>>>> pq;
+      
+      pq.push({0,src});
+      dis[src] = 0; 
+    
+      while(!pq.empty()){
+           auto it = pq.top();
+           pq.pop();
+           pair<ll,ll> cur = it.s;
+           int d = it.f;
+           
+           for(auto i:nodes){
+                 ll d = dist(i,cur);
+                 if(dis[cur]+d < dis[i]){
+                      dis[i] = dis[cur] + d;
+                      pq.push({dis[i],i});
+                 }
+           }
+          
+           for(auto i:mp[cur]){
+                 pair<ll,ll> temp = i.f;
+                 ll cost = i.s;
+               
+                 if(dis[cur] + cost < dis[temp]){
+                      dis[temp] = dis[cur] + cost;
+                      pq.push({dis[temp],temp}); 
+                 }
+           }
+           
+      }
+     cout<<dis[des]<<endl;
+      
 }
-
 int main() {
+    
     int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-
-        int x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2;
-
-        pii src = {x1, y1};
-        pii dest = {x2, y2};
-
-        vector<tuple<pii, pii, int>> wormholes;
-
-        for (int i = 0; i < n; i++) {
-            int a, b, c, d, cost;
-            cin >> a >> b >> c >> d >> cost;
-            wormholes.push_back({{a, b}, {c, d}, cost});
-        }
-
-        cout << solve(n, src, dest, wormholes) << endl;
+    cin>>t;
+    while(t--){
+         solve();
     }
     return 0;
 }
